2bb0a92ac52cbc4debb4512af27ac7b0d1626c8e
ShadowAdi <shadowshukla76@gmail.com>
2025-12-23 00:10:00 +0530

    Added Hooks to handle events

---
+++ events/hooks.ts
@@ -0,0 +1,50 @@
+import { CreateEventInput, UpdateEventInput } from "@/types/Event.types";
+import { createEvent, deleteEvent, getEventById, getEvents, updateEvent } from "@/utils/api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export const useEvents = () => useQuery({
+    queryKey: ["events"],
+    queryFn: getEvents
+})
+
+
+export const useEvent = (id: number) => useQuery({
+    queryKey: ["events", id],
+    queryFn: () => getEventById(id),
+    enabled: !!id,
+})
+
+export const useCreateEvent = () => {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: CreateEventInput) => createEvent(data),
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ["events"] });
+    },
+  });
+};
+
+
+export const useUpdateEvent = (id: number) => {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: UpdateEventInput) => updateEvent(id, data),
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ["events"] });
+      qc.invalidateQueries({ queryKey: ["events", id] });
+    },
+  });
+};
+
+export const useDeleteEvent = () => {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: (id: number) => deleteEvent(id),
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ["events"] });
+    },
+  });
+};
